1afec96e4913666d4f00d8ce7ce532f5f352dca1
Josh Holder <josh.holder@theguardian.com>
2017-05-22 16:23:08 +0100

    Split into desktop and mobile

--- src/render.js
+++ src/render.js
@@ -1,5 +1,12 @@
-import mainTemplate from './src/templates/main.html!text'
+import mobileTemplate from './src/templates/mobile.html!text'
+import desktopTemplate from './src/templates/desktop.html!text'
+
+import rp from 'request-promise'
+import Mustache from 'mustache'
 
 export async function render() {
-    return mainTemplate;
+	let spreadsheet = JSON.parse(await rp("https://interactive.guim.co.uk/docsdata-test/1_YBBoPBQE4sBV5rCxh8AhaLNiU1kPlBY30qCkVmZZWE.json"));
+	var renderData = {"spreadsheet": spreadsheet};
+	console.log(renderData)
+    return Mustache.render(mobileTemplate, renderData) + Mustache.render(desktopTemplate, renderData);
 }

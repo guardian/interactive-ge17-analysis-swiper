2585b08f442b5c9ede8baf7343d216b87b16d60e
Josh Holder <josh.holder@theguardian.com>
2017-05-24 13:31:24 +0100

    Remove test html

--- src/render.js
+++ src/render.js
@@ -14,34 +14,7 @@ export async function render(assetPath) {
         "spreadsheet": spreadsheet
     };
 
-    let table = `<table style="width:100%">
-          <tr>
-            <th>Firstname</th>
-            <th>Lastname</th> 
-            <th>Age</th>
-          </tr>
-          <tr>
-            <td>Jill</td>
-            <td>Smith</td> 
-            <td>50</td>
-          </tr>
-          <tr>
-            <td>Eve</td>
-            <td>Jackson</td> 
-            <td>94</td>
-          </tr>
-        </table>`;
-
-
-    let html = "";
-
-    for(let i = 0; i < 1; i++) {
-        html += table;
-    }
-
-    // return Mustache.render(mobileTemplate, renderData) + Mustache.render(desktopTemplate, renderData);
-
-    return html;
+    return Mustache.render(mobileTemplate, renderData) + Mustache.render(desktopTemplate, renderData);
 }
 
 function cleanGraphics(assetPath) {

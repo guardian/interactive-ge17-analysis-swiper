18f78201b1f6d62a2c5c65117e2f2047f9d1ef86
Josh Holder <josh.holder@theguardian.com>
2017-06-06 14:13:51 +0100

    Mobile hack attempt 1

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
 var el = document.createElement('script');
 el.src = '<%= path %>/app.js';
 document.body.appendChild(el);
+
+let mobile = document.querySelector(".swiper-no-swiping");
+
+window.addEventListener("scroll", () => {
+	if(window.scrollY > 48) {
+		mobile.classList.remove("swiper-no-swiping");
+	}
+})
